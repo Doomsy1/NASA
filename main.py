6551a9dc26ed481ec46b696ada7b9306c941af34
the-cryptic-coder <divyambanga@gmail.com>
2024-10-05 10:04:17 -0400

    added main

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import *
+from werkzeug.security import generate_password_hash,check_password_hash
+import sqlite3
+import random
+import time
+
+app=Flask(__name__)
+app.secret_key = "winners"
+logged_in = False
+
+@app.route('/')
+def home():
+    if logged_in:
+        return render_template("logged_in_home.html")
+    else:
+        return render_template("logged_out_home.html")
+
+
+if __name__ == "__main__":
+    app.run()
