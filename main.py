1c2af7adf1d213bd236f647a14735f64751531a4
Ario Ostovary <arioostov@gmail.com>
2024-10-05 10:25:02 -0400

    forecast template

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import *
-from werkzeug.security import generate_password_hash,check_password_hash
+# from werkzeug.security import generate_password_hash,check_password_hash
 import sqlite3
 import random
 import time
@@ -14,6 +14,13 @@ def home():
         return render_template("logged_in_home.html")
     else:
         return render_template("logged_out_home.html")
+    
+@app.route('/forecast')
+def forecast():
+    if logged_in:
+        return render_template("logged_in_home.html")
+    else:
+        return render_template("forecast.html")
 
 
 if __name__ == "__main__":
